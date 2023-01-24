59cb86fe673a23a4adb719c3572d71a7a8016c93
aritroCoder <ogcodinggod@gmail.com>
2023-01-25 02:17:26 +0530

    added a simple storage book snap

--- src/index.js
+++ src/index.js
@@ -1,18 +1,48 @@
 module.exports.onRpcRequest = async ({ origin, request }) => {
+  let state = await wallet.request({
+    method: 'snap_manageState',
+    params: ['get'],
+  });
+
+  if (!state) {
+    state = { book: [] };
+    // initialize state if empty and set default data
+    await wallet.request({
+      method: 'snap_manageState',
+      params: ['update', state],
+    });
+  }
+
   switch (request.method) {
+    case 'storeAddress':
+      state.book.push({
+        name: request.params.nameToStore,
+        address: request.params.addressToStore,
+      });
+      await wallet.request({
+        method: 'snap_manageState',
+        params: ['update', state],
+      });
+      return true;
+
     case 'hello':
+      let address_book = state.book
+        .map(function (item) {
+          return `${item.name}: ${item.address}`;
+        })
+        .join('\n');
       return wallet.request({
         method: 'snap_confirm',
         params: [
           {
             prompt: `Hello, ${origin}!`,
-            description:
-              'This custom confirmation is just for display purposes.',
-            textAreaContent:
-              'But you can edit the snap source code to make it do something, if you want to!',
+            description: 'Address book:',
+            textAreaContent: address_book,
           },
         ],
       });
+    case 'retrieveAddresses':
+      return state.book;
     default:
       throw new Error('Method not found.');
   }
